2a836160a056dc7e43bc40190526abc80c1371fc
NelsonPython <34105635+NelsonPython@users.noreply.github.com>
2019-12-06 15:13:52 -0800

    Add files via upload

---
+++ generiCode/selInput.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Firefox()
+driver.get("http://www.nelsontech.blog/Tutorial-SWVV/LogonDemo/login.html")
+
+usr = driver.find_element_by_id("username_field")
+pwd = driver.find_element_by_id("password_field")
+btn = driver.find_element_by_id("login_button")
+
+usr.send_keys('demo', Keys.ARROW_DOWN)
+pwd.send_keys('mode', Keys.ARROW_DOWN)
+btn.click()
+
+if driver.title == "Welcome":
+	print("Test passed")
+else:
+        print("Test failed ", driver.title)
+
+driver.quit()
