9faa16b0152d81e3bc1da0f125178b5c639bae91
NelsonPython <34105635+NelsonPython@users.noreply.github.com>
2019-12-06 16:32:03 -0800

    Update 01_generic_web_input_form.py

--- code/01_generic_web_input_form.py
+++ code/01_generic_web_input_form.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 driver = webdriver.Firefox()
 driver.get("http://www.nelsontech.blog/Tutorial-SWVV/LogonDemo/login.html")
